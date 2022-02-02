b931871fc5c9206a009e6ba2090bd150755d8404
Zak <pruittzn@gmail.com>
2022-02-02 12:40:45 -0600

    successfully parse out 6 letter words and assign values to them in six_letter_words.json

--- word_parser.py
+++ word_parser.py
@@ -1,14 +1,52 @@
 import json
 
+letter_value = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 1,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 1,
+    'j': 8,
+    'k': 9,
+    'l': 10,
+    'm': 11,
+    'n': 12,
+    'o': 1,
+    'p': 13,
+    'q': 14,
+    'r': 15,
+    's': 16,
+    't': 17,
+    'u': 1,
+    'v': 18,
+    'w': 19,
+    'x': 20,
+    'y': 1,
+    'z': 21
+}
 
-with open('words.json') as words_file, open("six_letter_words.json", "w") as six_letter_words_file:
-    six_letter_words_file.write('{')
-    data = json.load(words_file)
-    for key, value in data.items():
+
+def calculate_word_value(word):
+    value = 0
+    for letter in word:
+        value += letter_value[letter]
+    return value
+
+
+with open('words.json') as words_file, open("six_letter_words.json", "r+") as six_letter_words_file:
+    word_json = json.load(words_file)
+    six_letter_json = json.load(six_letter_words_file)
+    for key, value in word_json.items():
         if (len(key) == 6):
-            wordJSON ={
-                key : value,
-                "value" : 1,
+            wordJSON = {
+                key: value,
+                "value": calculate_word_value(key),
             }
-            json.dump(wordJSON, six_letter_words_file)
-    six_letter_words_file.write('}')
+            six_letter_json.append(wordJSON)
+    json.dump(six_letter_json, six_letter_words_file, 
+                        indent=4,  
+                        separators=(',',': '))
