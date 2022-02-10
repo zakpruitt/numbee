9f663a22ba71edd8642a1ae7e83cb157358e5ee5
Zak <pruittzn@gmail.com>
2022-02-10 12:50:26 -0600

    added in scheduler to pick a new word each day. also added a get route to retrieve word

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
+import time
+import atexit
+
 from flask import Flask, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+from word_parser import select_word
+
 
 app = Flask(__name__)
+word = None
+
+#### select and schedule a new word daily ####
+def select_new_word():
+    if (time.strftime("%I%p") == "12AM"):
+        global word
+        word = select_word()
+    else:
+        print("Not time to select a new word")
 
 
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=select_new_word, trigger="interval", seconds=120)
+scheduler.start() 
+atexit.register(lambda: scheduler.shutdown())
+
+#### routes ####
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+@app.route("/word")
+def get_word():
+    global word
+    if word == None:
+        word = select_word()
+    return word
+
+#### main function ####
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
