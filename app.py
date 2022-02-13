d93ff54481da1e7b0d392e11df43fd8427a14c53
Zak Pruitt <pruittzn@gmail.com>
2022-02-13 11:57:39 -0600

    desktop handling

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 from flask import Flask, render_template
 from apscheduler.schedulers.background import BackgroundScheduler
-from word_parser import select_word, get_all_words, delete_word
+from word_parser import letter_value, select_word, get_all_words, delete_word
 
 
 app = Flask(__name__)
@@ -20,7 +20,7 @@ def select_new_word():
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=select_new_word, trigger="interval", seconds=120)
-scheduler.start() 
+scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
 #### routes ####
@@ -28,6 +28,9 @@ atexit.register(lambda: scheduler.shutdown())
 def home():
     return render_template("index.html")
 
+@app.route("/not-supported")
+def not_supported():
+    return render_template("not_supported.html")
 
 @app.route("/word")
 def get_word():
@@ -40,6 +43,11 @@ def get_word():
 @app.route("/words")
 def get_words():
     return get_all_words()
+
+
+@app.route("/values")
+def get_values():
+    return json.dumps(letter_value)
 
 #### main function ####
 if __name__ == '__main__':
